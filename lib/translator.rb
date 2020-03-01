2b0413da63e59dd4d25815e1de6f6ae68f0f65c6
Colton stephens <stephens.colton@icloud.com>
2020-03-01 03:39:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,37 +1,54 @@
-# require modules here
-require "yaml"
+require 'yaml'
 require 'pry'
 
 def load_library
   # code goes here
 def load_library(file_path)
-  files = YAML.load_file(file_path)
-  result = {"get_meaning" => {}, "get_emoticon" => {}}
-  files.each do |meaning, emoticon|
-    result["get_meaning"][emoticon[1]] = meaning
-    result["get_emoticon"][emoticon[0]] = emoticon[1]
-  end
-  result
+  emote_list = YAML.load_file(file_path)
+  get_meaning = {}
+  get_emoticon = {}
+
+  emote_list.each do |meaning, emotes_array|
+    get_meaning[emotes_array[1]] = meaning
+  end 
+
+  emote_list.each do |meaning, emotes_array|
+    get_emoticon[emotes_array[0]] = emotes_array[1]
+  end 
+  result = {:get_meaning => get_meaning, :get_emoticon => get_emoticon}
 end
 
 def get_japanese_emoticon
   # code goes here
 def get_japanese_emoticon(file_path, emoticon)
-  emo = load_library(file_path)
-  jap_emo = emo["get_emoticon"][emoticon]
-  if jap_emo
-    jap_emo
-  else 
-    "Sorry, that emoticon was not found"
+  library = load_library(file_path)
+  found = false
+  library[:get_emoticon].each do |english, japanese|
+    if emoticon == english
+      emoticon = japanese
+      found = true
+    end
   end
-end 
-
-def get_english_meaning(file_path, emoticon)
-  emo = load_library(file_path)
-  eng_meaning = emo["get_meaning"][emoticon]
-  if eng_meaning
-    eng_meaning
-  else
-    "Sorry, that emoticon was not found"
+  if found == true
+    return emoticon 
+  elsif found == false
+    return "Sorry, that emoticon was not found"
   end
 end
+
+def get_english_meaning
+  # code goes here
+def get_english_meaning(file_path, emoticon)
+  library = load_library(file_path)
+  found = false
+  library[:get_meaning].each do |japanese, english_meaning|
+    if emoticon == japanese
+      emoticon = english_meaning
+      found = true
+    end
+  end
+  if found == true
+    return emoticon
+  elsif found == false
+    return "Sorry, that emoticon was not found"
+  end 

54262803fe445eb2fed46c13d1cd6d9f5127d793
Colton stephens <stephens.colton@icloud.com>
2020-03-01 03:34:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,4 +7,21 @@ def load_library(file_path)
   end
   result
   
-  
+  def get_japanese_emoticon(file_path, emoticon)
+  emo = load_library(file_path)
+  jap_emo = emo["get_emoticon"][emoticon]
+  if jap_emo
+    jap_emo
+  else 
+    "Sorry, that emoticon was not found"
+  end
+end 
+
+def get_english_meaning(file_path, emoticon)
+  emo = load_library(file_path)
+  eng_meaning = emo["get_meaning"][emoticon]
+  if eng_meaning
+    eng_meaning
+  else
+    "Sorry, that emoticon was not found"
+  end
